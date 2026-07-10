3ca006cc7772b56ffbe11eeaa7340f66de42ebe8
Kevin Sandermann <Kevin.sandermann@gmail.com>
2026-07-10 23:22:53 +0200

    Add provider and Terraform core constraints for Azure workspace

---
+++ azure-public-vm-hello-world/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.100.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  version = ">= 3.100.0"
+
+  features {}
+
+  subscription_id = "00000000-0000-0000-0000-000000000000"
+  tenant_id       = "00000000-0000-0000-0000-000000000000"
+  client_id       = "00000000-0000-0000-0000-000000000000"
+  client_secret   = "replace-with-service-principal-secret"
+}
