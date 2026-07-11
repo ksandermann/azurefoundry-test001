7d2b4a526269e18a68c0d1613fd326185e3d9159
Kevin Sandermann <Kevin.sandermann@gmail.com>
2026-07-11 08:41:45 +0200

    Fix workspace outputs to match module name

--- workspaces/azure-public-vm-hello/outputs.tf
+++ workspaces/azure-public-vm-hello/outputs.tf
@@ -1,11 +1,11 @@
 output "resource_group" {
-  value = module.public-vm.resource_group
+  value = module.public_vm.resource_group
 }
 
 output "public_ip" {
-  value = module.public-vm.public_ip
+  value = module.public_vm.public_ip
 }
 
 output "virtual_machine" {
-  value = module.public-vm.virtual_machine
+  value = module.public_vm.virtual_machine
 }
