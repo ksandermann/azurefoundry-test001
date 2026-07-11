faa93491e10df5c6ff8125427c1c45f3efc14b00
Kevin Sandermann <Kevin.sandermann@gmail.com>
2026-07-11 08:51:27 +0200

    feat: add module outputs for azure linux vm

---
+++ modules/azure-linux-vm-public/outputs.tf
@@ -0,0 +1,15 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.this.name
+}
+
+output "virtual_machine_name" {
+  value = azurerm_linux_virtual_machine.this.name
+}
+
+output "public_ip_address" {
+  value = azurerm_public_ip.this.ip_address
+}
+
+output "public_ip_fqdn" {
+  value = azurerm_public_ip.this.fqdn
+}
