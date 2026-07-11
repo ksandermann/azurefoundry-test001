932543fc8e1866ba63a65cbd5b5386e48b5af9dc
Kevin Sandermann <Kevin.sandermann@gmail.com>
2026-07-11 09:05:14 +0200

    Add explicit provider configuration for Azure workspace

---
+++ azure-hello-vm/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  environment     = var.environment
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+}
