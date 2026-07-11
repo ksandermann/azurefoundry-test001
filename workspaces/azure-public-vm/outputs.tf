01a6705c24a9cd1045fbafbb10853520a9ee5622
Kevin Sandermann <Kevin.sandermann@gmail.com>
2026-07-11 09:03:15 +0200

    Fix workspace output module references

--- workspaces/azure-public-vm/outputs.tf
+++ workspaces/azure-public-vm/outputs.tf
@@ -1,11 +1,11 @@
 output "resource_group" {
-  value = module.azure-public-vm.resource_group
+  value = module.azure_public_vm.resource_group
 }
 
 output "virtual_machine" {
-  value = module.azure-public-vm.virtual_machine
+  value = module.azure_public_vm.virtual_machine
 }
 
 output "public_ip_address" {
-  value = module.azure-public-vm.public_ip_address
+  value = module.azure_public_vm.public_ip_address
 }
